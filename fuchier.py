6971092f7fce169401aee2a198ea4e4e62c074c6
Mathieu <mathieusin2005@gmail.com>
2024-03-15 11:34:02 +0100

    carte test 1.1

--- fuchier.py
+++ fuchier.py
@@ -69,7 +69,7 @@ while len(city_coordinates) < num_cities:
 
 # Add city names to the plot
 for city_coord, city_name in zip(city_coordinates, city_names):
-    plt.text(city_coord[1], city_coord[0], city_name, color='black', fontsize=12, ha='center', va='center', fontname='Jokerman')
+    plt.text(city_coord[1], city_coord[0], city_name, color='black', fontsize=13, ha='center', va='center', fontname='Jokerman')
 
 # Display the colored heightmap in 2D
 plt.imshow(colored_heightmap, origin='lower')
