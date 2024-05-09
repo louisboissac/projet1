88accd34b1348e51e4244de463fab58e82fc48f6
Mathieu <mathieusin2005@gmail.com>
2024-05-09 14:05:41 +0200

    carte avec les fonctions

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from carte_avc_modif import generate_map
 app = Flask(__name__)
 
 # Définir la route pour la page d'accueil
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Récupérer les valeurs des paramètres depuis le formulaire
@@ -23,10 +24,10 @@ def index():
         carte_image = generate_map(taille_carte, niv_eau, niv_montagne, niv_plaine, niv_foret, nombre_villes)
 
         # Rendre le template HTML de la page d'accueil avec l'image de la carte
-        return render_template('interface.html', carte_image=carte_image)
+        return render_template('interface2.html', carte_image=carte_image)
     else:
         # Si c'est une requête GET, simplement afficher le formulaire
-        return render_template('interface.html')
+        return render_template('interface2.html')
 
 # Exécuter l'application Flask
 if __name__ == '__main__':
